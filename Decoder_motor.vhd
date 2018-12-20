79cb34d14f99b1e5b6e6ffec71f58ba43471d568
MarcBalle <45721497+MarcBalle@users.noreply.github.com>
2018-12-20 22:54:47 +0100

    Update Decoder_motor.vhd

--- Decoder_motor.vhd
+++ Decoder_motor.vhd
@@ -6,27 +6,27 @@ use ieee.numeric_std.all;
 use std.textio.all;
 use ieee.std_logic_textio.all;
 
-Entity decoder is 
+Entity decoder_motor_led is 
 
 
 	Port (
 
-		motor : IN std_logic_vector(1 downto 0);
+		motor_ascensor : IN std_logic_vector(1 downto 0);
 		Led : OUT std_logic_vector(6 downto 0)
 
 		);
 
 End entity; 
 
-Architecture dataflow of decoder is 
+Architecture dataflow of decoder_motor_led is 
 
 Begin 
 	
-With motor select 
+With motor_ascensor select 
 			
-			Led <= "0000001" when "00",
-				   "1001111" when "01",
-				   "0010010" when others;
+			Led <= "0000001" when "10", -- Hacia arriba
+				   "1001111" when "01",-- Hacia abajo 
+				   "0010010" when others; --Parado
 
 
 End architecture;
@@ -37,33 +37,33 @@ End architecture;
 
 
 
-Entity decoder_tb is 
+Entity decoder_motor_led_tb is 
 
-End decoder_tb;
+End decoder_motor_led_tb;
 
 
 
-Architecture behavioral of decoder_tb is 
+Architecture behavioral of decoder_motor_led_tb is 
 
-	Component decoder is 
+	Component decoder_motor_led is 
 
 
 		Port (
 
-			motor : IN std_logic_vector(1 downto 0);
+			motor_ascensor: IN std_logic_vector(1 downto 0);
 			Led : OUT std_logic_vector(6 downto 0)
 
 			);
 
 	End component;
 
-Signal motor : std_logic_vector(1 downto 0);
+Signal motor_ascensor : std_logic_vector(1 downto 0);
 Signal Led: std_logic_vector(6 downto 0);
 
 
 TYPE vtest is record
 
-	 motor : std_logic_vector(1 DOWNTO 0);
+	 motor_ascensor : std_logic_vector(1 DOWNTO 0);
 	 Led : std_logic_vector(6 DOWNTO 0);
 
 END RECORD;
@@ -73,19 +73,19 @@ TYPE vtest_vector IS ARRAY (natural RANGE <>) OF vtest;
 
 CONSTANT test: vtest_vector := (
 
- (motor => "00", led => "0000001"),
- (motor => "01", led => "1001111"),
- (motor => "10", led => "0010010")
+ (motor_ascensor => "00", led => "0000001"),
+ (motor_ascensor => "01", led => "1001111"),
+ (motor_ascensor => "10", led => "0010010")
 
  );
 
 
  Begin 
 
- 		uut: decoder port map (
+ 		uut: decoder_motor_led port map (
  			
- 			motor => motor;
- 			Led => Led;
+ 			motor_ascensor => motor_ascensor,
+ 			Led => Led
  		);
  			
  		       
@@ -95,16 +95,16 @@ CONSTANT test: vtest_vector := (
 
 				BEGIN
 						FOR i IN 0 TO test'HIGH LOOP
-							motor <= test(i).motor;
+							motor_ascensor <= test(i).motor_ascensor;
 							WAIT FOR 20 ns;
 							ASSERT led = test(i).led
-									REPORT "Salida incorrecta."
-									SEVERITY FAILURE;
+								REPORT "Salida incorrecta."
+								SEVERITY FAILURE;
 						END LOOP;
 
 						ASSERT false
 
-						REPORT "Simulacin finalizada. Test superado."
+						REPORT "Simulacion finalizada. Test superado."
 						SEVERITY FAILURE;
            
             End process;
